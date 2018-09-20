01eda575d37554a13f37312de6dfd9c96401c6ec
Trix <trbelleza@up.edu.ph>
2018-09-20 21:54:44 +0800

    Added return labels to exer5

--- belleza_exer5.r
+++ belleza_exer5.r
@@ -3,9 +3,8 @@ setwd(this.dir)
 
 source("belleza_exer4.r")
 size = length(GaussianResult$solutionSet)
-#x <- c(50,50,50,70,70,70,80,80,80,90,90,90,100,100,100)
-#y <- c(3.3,2.8,2.9,2.3,2.6,2.1,2.5,2.9,2.4,3.0,3.1,2.8,3.3,3.5,3.0)
 PolynomialRegression <- function(size) {
+  
   polynomial = ""
   f = "f <- function(x)"
   for(i in 1:size) {
@@ -16,10 +15,9 @@ PolynomialRegression <- function(size) {
     }
   
   }
-  #print(polynomial)
-  eval(parse(text = paste(f,polynomial, sep=" ")))
-  #print(f(10))
-  
+  print(polynomial)
+  s = eval(parse(text = paste(f,polynomial, sep=" ")))
+  return(list(coefficients = GaussianResult$solutionSet, func = s))     
 }
-
-print(PolynomialRegression())
+a = PolynomialRegression(size)
+print(a$func(10))
