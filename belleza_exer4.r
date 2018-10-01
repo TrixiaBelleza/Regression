4eb3059b407ad8f69b1f77bbd398f066d607bde7
Trix <trbelleza@up.edu.ph>
2018-10-01 22:30:45 +0800

    Updated files

--- belleza_exer4.r
+++ belleza_exer4.r
@@ -1,13 +1,13 @@
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
-source("getCoeffMatrix.r")
+source("belleza_exer3.r")
 
 #Import matrix from exer 3
-result = getACM()
-acm = result$augcoeffmatrix
-vars = result$variables
-varCount = length(vars)
+#result = getACM()
+#acm = result$augcoeffmatrix
+#vars = result$variables
+#varCount = length(vars)
 
 #Gets the pivot row based on the given max value in column
 getPivotRow <- function(acm, currCol, rowNum, max_in_col_i) {
@@ -43,7 +43,8 @@ Gaussian <- function(acm, varCount) {
       acm[j,] = acm[j,] - norm
     }
   }
-  #print(acm)
+ # print("luh")
+#  print(acm)
   #Store x[n] to list kasi may value na siya.
   x <- c()
   lastVar = acm[varCount, varCount+1] / acm[varCount, varCount]
@@ -62,8 +63,9 @@ Gaussian <- function(acm, varCount) {
     coeffWITHunknown = acm[i,i]
     x[i] = (b[i] - sum(coeffsCROSSknows)) / coeffWITHunknown
   }
-  #print("[Gaussian]:")
-  return(list(solutionSet = x, variables = vars, matrix = acm))     
+  print("[Gaussian]:")
+  print(acm)
+  return(list(solutionSet = x, matrix = acm))     
 }
 
 GaussJordan <- function(acm, varCount) {
@@ -104,7 +106,7 @@ GaussJordan <- function(acm, varCount) {
 } 
 
 #print("Gaussian: ")
-GaussianResult = Gaussian(acm, varCount)
+#GaussianResult = Gaussian(acm, varCount)
 #print(Gaussian(acm, varCount))
 #print("Gauss-Jordan: ")
 #print(GaussJordan(acm, varCount))
--- belleza_exer5.r
+++ belleza_exer5.r
@@ -2,22 +2,78 @@ this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
 source("belleza_exer4.r")
-size = length(GaussianResult$solutionSet)
-PolynomialRegression <- function(size) {
-  
+
+getRegColNames <- function(size) {
+  colnames <- c()
+  for (i in 1:(size+1)) {
+    #concat x and number i
+    colname = paste("a",i, sep="")
+    colnames <- c(colnames, colname)  
+  }
+  colnames <- c(colnames, "RHS") #adds RHS at the last index of the vector
+  return(colnames)
+}
+#returns -> c(1, 2, 3, ..., n)
+getRegRowNames <- function(size) {
+  rownames <- c()
+  for (i in 1:(size+1)) {
+    rownames <- c(rownames, i)      
+  }
+  return(rownames)
+}
+getACM <- function(x,y) {
+  if((length(x) != length(y)) | (size < 1)){
+    return(NA)
+  }
+  else{
+    #declare matrix
+    vars = getRegColNames(size)[1:(size+1)]
+    augcoeffmatrix = matrix(data=NA, nrow=size+1, ncol=size+2, dimnames=list(getRegRowNames(size), getRegColNames(size)))
+    RHScount = 0
+    for(i in 1:(size+1)) { #for each row
+      count = i-1
+      for(j in 1:(size+2)){ #for each column
+        if(j == (size+2)) {
+          augcoeffmatrix[i,j] = sum((x**RHScount) * y)
+          RHScount = RHScount + 1
+        }
+        else {
+          augcoeffmatrix[i,j] = sum(x**count)
+          count = count + 1
+        }
+      }
+    }
+    #print(augcoeffmatrix)
+    return(list(variables = vars, augcoeffmatrix = augcoeffmatrix))
+  }
+}
+
+PolynomialRegression <- function(size,x,y) {
+  #size = degree
   polynomial = ""
   f = "f <- function(x)"
-  for(i in 1:size) {
-    if(i!=size) {
+  a = getACM(x,y)
+  acm = a$augcoeffmatrix
+  print("Acm")
+  print(acm)
+  print(size)
+  GaussianResult =  Gaussian(acm, size+1)
+  print(GaussianResult)
+  for(i in 1:(size+1)) {
+    if(i!=(size+1)) {
       polynomial = paste(polynomial, " ", GaussianResult$solutionSet[[i]]," * x**", (i-1), " + ", sep="")
     }else{
       polynomial = paste(polynomial, " ", GaussianResult$solutionSet[[i]]," * x**", (i-1), "", sep="")
     }
-  
   }
-  print(polynomial)
   s = eval(parse(text = paste(f,polynomial, sep=" ")))
+
   return(list(coefficients = GaussianResult$solutionSet, func = s))     
 }
-a = PolynomialRegression(size)
-print(a$func(10))
+x <- c(50,50,50,70,70,70,80,80,80,90,90,90,100,100,100)
+y <- c(3.3,2.8,2.9,2.3,2.6,2.1,2.5,2.9,2.4,3.0,3.1,2.8,3.3,3.5,3.0)
+size = 3
+
+regression = PolynomialRegression(size,x,y)
+print(regression$func(100))
+print(regression$coefficients)
